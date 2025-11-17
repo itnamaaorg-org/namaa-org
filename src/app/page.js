ce5221aeff8f5565e7c44d296eea6d0abc139ac0
ahmad taher <ahmadtaher@ahmads-MacBook-Pro.local>
2025-11-17 18:25:29 +0300

    all section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Header from '@/app/components/Header'
 import Partners from './components/Partner';
 import Ourjourney from './components/Ourjourney'
+import Achievements from './components/Achievements';
+import Footer from './components/Footer';
+import Impactsection from './components/Impactsection';
 
+import Head from 'next/head';
 
 
 
@@ -9,11 +13,28 @@ const Home = () => {
   
   return (
 <div>
+        <Head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800;900&display=swap" 
+          rel="stylesheet"
+        />
+        <style>{`
+          body {
+            font-family: 'Tajawal', sans-serif;
+            overflow-x: hidden;
+          }
+        `}</style>
+      </Head>
+
    <Header></Header>
-<div style={{ height: '350px', paddingTop: '110px' ,backgroundColor:'#F9F9F9', textAlign:'center', fontSize:'30px'}}>ahmad</div>
+   <Impactsection></Impactsection>
 
     <Partners></Partners>
     <Ourjourney></Ourjourney>
+    <Achievements></Achievements>
+    <Footer></Footer>
+
+
     </div>
    
   );
